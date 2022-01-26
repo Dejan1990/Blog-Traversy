7ab97bef710acd34f2d886d2b78bd75298bdbb76
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-26 18:35:19 +0100

    Loggin in

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/posts', function () {
@@ -13,3 +14,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
