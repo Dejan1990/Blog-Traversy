844c316c2dd76028843eedffb40acbb4d7eef995
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-26 18:56:57 +0100

    Loggin out

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/posts', function () {
     return view('posts.index');
@@ -17,3 +22,5 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
