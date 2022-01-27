a72e9941f23f17f6330f383d1560bec2e669db74
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-27 14:39:02 +0100

    Setting Up Posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+       /* $request->user()->posts()->create($request->validate([
+            'body' => 'required'
+        ])); */
+       /* Post::create([
+           'user_id' => auth()->id(), // bilo bi neophodno dodati 'user_id' u $fillable u Post.php
+           'body' => $request->body
+       ]); */
+
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -24,3 +25,6 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::post('/posts', [PostController::class, 'store']);
