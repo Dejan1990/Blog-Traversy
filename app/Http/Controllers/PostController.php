8141e8d4cfd5692791ff2dd00e3e1fbf6877071c
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-27 16:12:28 +0100

    Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->with('user')->paginate(20);
+        $posts = Post::latest()->with('user')->simplePaginate(2);
 
         return view('posts.index', [
             'posts' => $posts
