717c0af0b93b6c38fc7e509ff906a532e6e45f75
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-27 16:23:54 +0100

    Seeding With Model Factories

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -14,7 +14,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'body' => $this->faker->sentence(rand(20, 24)),
         ];
     }
 }
