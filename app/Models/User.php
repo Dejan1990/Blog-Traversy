b2ef7649e3ddb97fe6349585a55ba224b715e9cc
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-28 15:38:20 +0100

    Like Count Eloquent Relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
+
+    public function receivedLikes()//relationship to access things from a distant model
+    {
+        return $this->hasManyThrough(Like::class, Post::class);
+    }
 }
