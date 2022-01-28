ca028cbf0a99f9596fdd4a7ff9dd5228abe29815
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-28 12:18:57 +0100

    Eager Loading

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->with('user')->simplePaginate(20);
+        $posts = Post::latest()->with(['user', 'likes'])->simplePaginate(20);
 
         return view('posts.index', [
             'posts' => $posts
